2f5b19175618a5866978be5a4358932a028c5b18
Ajinkya2706 <ajinkyanikumbh91@gmail.com>
2026-04-25 09:33:53 +0530

    changed serverseletiontimeout

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -6,7 +6,7 @@ async function connectToDB() {
 
     try {
         await mongoose.connect(process.env.MONGO_URI, {
-            serverSelectionTimeoutMS: 5000,
+            serverSelectionTimeoutMS: 10000,
             dnsSrvOrder: false
         })
 
